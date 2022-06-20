a23a085c8586afd7fe4a60841cb196ccbc24af7c
Sahil <sahilsingh7977@gmail.com>
2022-06-20 16:53:19 +0530

    added traversal function

--- stack.c
+++ stack.c
@@ -1,6 +1,28 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+// adding structure of linked list to create stack
+struct node {
+    int n;
+    struct node*ptr;  
+};
+
+void traversal(struct node *a){
+while(a!=NULL){
+    printf("%d",a->n);
+    a=a->ptr;
+    printf("\n");
+}
+}
 
 int main (){
-
+    struct node *head,*body;
+    head=(struct node*)malloc(sizeof(struct node));
+    body=(struct node*)malloc(sizeof(struct node));
+    head->n=10;
+    head->ptr=body;
+    body->n=20;
+    body->ptr=NULL;
+    traversal(head);
     return 0;
 }
